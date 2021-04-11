9ca9210409121d1f629c60cc548a43a83e13c81a
jimpaly <64745560+jimpaly@users.noreply.github.com>
2021-04-11 15:11:17 -0700

    🔁` Changed clock to functional component
    Also disabled text selection

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -8,31 +8,23 @@ interface ClockState {
   time: Date;
 }
 
-export class Clock extends React.PureComponent<ClockProps, ClockState> {
-  interval: NodeJS.Timeout | null;
-  constructor(props: ClockProps) {
-    super(props);
-    this.interval = null;
-    this.state = {
-      time: new Date(),
-    };
-  }
-  componentDidMount() {
-    this.interval = setInterval(() => {
+export const Clock: React.FC<ClockProps> = (props: ClockProps) => {
+  const [time, setTime] = React.useState<Date>(new Date());
+  let interval: NodeJS.Timeout | null = null;
+
+  React.useEffect(() => {
+    interval = setInterval(() => {
       const newTime = new Date();
-      if (newTime.getMinutes() !== this.state.time.getMinutes()) this.props.updateBackground();
-      this.setState({
-        time: newTime,
-      });
+      if (newTime.getMinutes() !== time.getMinutes()) props.updateBackground();
+      setTime(newTime);
     }, 100);
-  }
-  componentWillUnmount() {
-    if (this.interval) clearInterval(this.interval);
-  }
-  render() {
-    return <div className="clock">{dateToString(this.state.time)}</div>;
-  }
-}
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, []);
+
+  return <div className="clock">{dateToString(time)}</div>;
+};
 
 function dateToString(date: Date) {
   return date.toLocaleTimeString();
